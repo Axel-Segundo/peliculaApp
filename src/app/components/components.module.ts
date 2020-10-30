328bc2cd0e3302fbf1686bb669789b981e45cfc5
Axel Segundo <axeljasegundo@gmail.com>
2020-10-30 11:16:18 -0600

    254-258

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { RatingModule } from 'ng-starrating';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
+import { PipesModule } from '../pipes/pipes.module';
+
 
 
 @NgModule({
@@ -19,7 +22,9 @@ import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    RatingModule,
+    PipesModule
   ]
 })
 export class ComponentsModule { }
--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,18 +1,54 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { CarteleraResponse } from '../interfaces/cartelera-response';
+import { Observable, of } from 'rxjs';
+import {map, tap } from 'rxjs/operators';
+import { CarteleraResponse, Movie } from '../interfaces/cartelera-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculasService {
-
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private carteleraPage = 1;
+  public cargando: boolean = false;
   constructor(private http: HttpClient) { }
 
-  getCartelera():Observable<CarteleraResponse> {
-
-    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=fe88e5740d2c9aeb9d905af84a4b4787&language=es-ES&page=1')
-
+  get params(){
+    return{
+      api_key:'fe88e5740d2c9aeb9d905af84a4b4787',
+      language:'es-ES',
+      page:this.carteleraPage.toString()
+    }
   }
+
+  getCartelera():Observable<Movie[]> {
+
+    if (this.cargando) {
+      //cargando peliculas
+      return of([]);
+    }
+
+    this.cargando = true;
+
+    return this.http.get<CarteleraResponse>(`${ this.baseUrl }/movie/now_playing`,{
+      params: this.params
+    }).pipe(
+      map((resp) => resp.results),
+      tap( () => {
+        this.carteleraPage +=1;
+        this.cargando = false;
+      })
+    );
+    }
+    buscarPeliculas(texto: string):Observable<Movie[]>{
+
+      const params = {...this.params, page: '1', query: texto};
+      //https://api.themoviedb.org/3/search/movie
+      return this.http.get<CarteleraResponse>(`${this.baseUrl}/search/movie`,{
+        params
+      }).pipe(
+        map(resp => resp.results)
+      )
+  }
+
 }
